e6bd53d07145ef57d8ba22d5897058445ba776ac
PraveenChand <impraveenchand@gmail.com>
2021-09-17 10:55:51 +0530

    Added new line of code to main

--- src/com/JavaDemoGit/Main.java
+++ src/com/JavaDemoGit/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("This is my First Git Project with IntelliJ");
+        System.out.println("Testing with code change");
     }
 }
