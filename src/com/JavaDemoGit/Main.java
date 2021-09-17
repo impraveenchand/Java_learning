5e472770e413daf90bfacd9a926c5c9e340bc8f1
PraveenChand <impraveenchand@gmail.com>
2021-09-17 10:27:35 +0530

    My First Git Commit of the project

---
+++ src/com/JavaDemoGit/Main.java
@@ -0,0 +1,9 @@
+package com.JavaDemoGit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("This is my First Git Project with IntelliJ");
+    }
+}
